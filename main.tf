dcfdc61f87711b3d5729be761329584dfd2c8d63
ashifanassar <ashifanassar@gmail.com>
2024-05-04 22:46:28 +0530

    modules

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# module "alb-public" {
-#     source                  = "./vendor/modules/alb/"
-#     ENV                     = var.ENV
-#     ALB_NAME                = "roboshop-${var.ENV}-public"
-#     INTERNAL                = false 
-# }
+module "alb-public" {
+    source                  = "./vendor/modules/alb/"
+    ENV                     = var.ENV
+    ALB_NAME                = "roboshop-${var.ENV}-public"
+    INTERNAL                = false 
+}
 
 
-# module "alb-private" {
-#     source                  = "./vendor/modules/alb/"
-#     ENV                     = var.ENV
-#     ALB_NAME                = "roboshop-${var.ENV}-private"
-#     INTERNAL                = true 
+module "alb-private" {
+    source                  = "./vendor/modules/alb/"
+    ENV                     = var.ENV
+    ALB_NAME                = "roboshop-${var.ENV}-private"
+    INTERNAL                = true 
 
-# }
+}

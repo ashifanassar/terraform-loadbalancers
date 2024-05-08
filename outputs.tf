9a864cacd69f75ecae757e833f56d3a063c1f609
ashifanassar <ashifanassar@gmail.com>
2024-05-08 20:50:35 +0530

    module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,38 @@
+output "PUBLIC_ALB_ARN" {
+  value = module.alb-public.ALB_ARN
+}
+
+output "PRIVATE_ALB_ARN" {
+  value = module.alb-private.ALB_ARN
+}
+
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb-public.ALB_ADDRESS
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb-private.ALB_ADDRESS
+}
+
+output "PRIVATE_LISTENER_ARN" {
+    value =  module.alb-private.PRIVATE_LISTENER_ARN
+} 
+
+output "PUBLIC_LISTENER_ARN" {
+    value =  module.alb-public.PUBLIC_LISTENER_ARN
+} 
+
+
+
+
+
+
+
+
+
+
+
+
 # output "VPC_ID" {
 #     value = module.vpc.VPC_ID 
 # }
